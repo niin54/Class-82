2a5c3cd7e8e17536092b7e7c5c81d0967418075b
niin54 <82283459+niin54@users.noreply.github.com>
2022-01-22 08:40:55 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ var prevx, prevy, mouseevent;
 var color = "black";
 var width = 2;
 
-canvas.addEventListener("mousedown", mouseDown);
-function mouseDown(e) {
+canvas.addEventListener("mousedown", my_mouseDown);
+function my_mouseDown(e) {
     color=document.getElementById("color").value;
     width=document.getElementById("width").value;
-    mouseevent= "mouse_down";
+    mouseevent= "mouseDown";
 }
-canvas.addEventListener("mouseleave", mouseLeave);
-function mouseLeave(e) {
-    mouseevent="mouse_leave";
+canvas.addEventListener("mouseleave", my_mouseLeave);
+function my_mouseLeave(e) {
+    mouseevent="mouseLeave";
 }
-canvas.addEventListener("mouseup",mouseUp);
-function mouseUp(e) {
-    mouseevent="mouse_up";
+canvas.addEventListener("mouseup",my_mouseUp);
+function my_mouseUp(e) {
+    mouseevent="mouseUP";
 }
-canvas.addEventListener("mousemove",mouseMove);
-function mouseMove(e) {
+canvas.addEventListener("mousemove",my_mouseMove);
+function my_mouseMove(e) {
     mousex=e.clientX-canvas.offsetLeft;
     mousey=e.clientY-canvas.offsetTop;
-    if (mouseevent="mouse_down") {
+    if (mouseevent="mouseDown") {
         ctx.beginPath();
         ctx.strokeStyle=color;
         ctx.lineWidth=width;
@@ -31,7 +31,7 @@ function mouseMove(e) {
         ctx.moveTo (prevx,prevy);
         console.log("Current x= "+mousex+" Current y= "+mousey);
         ctx.lineTo (mousex,mousey);
-        ctx.stroke()
+        ctx.stroke();
     }
     prevx=mousex;
     prevy=mousey;
